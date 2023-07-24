c820938465ce21cc66678728124e86b0e7a68777
Andrew-C-Duong <140382522+Andrew-C-Duong@users.noreply.github.com>
2023-07-24 18:36:45 +1000

    Add comments OrganiseFilesbyDate.ps1
    Added comments to code

--- OrganiseFilesbyDate.ps1
+++ OrganiseFilesbyDate.ps1
@@ -1,9 +1,15 @@
-$ParentDir  = "G:\PhoneVids\*"
-$DestDir = "G:\PhoneVideos"
-$Files = Get-ChildItem -path $ParentDir -Recurse
+# Folder containing videos to be sorted
+$ParentDir  = "G:\PhoneVids\*" 
 
+# Folder videos should be moved into
+$DestDir = "G:\PhoneVideos"
+
+
+# Recursive function to rename files based on time and date metadata of video
+$Files = Get-ChildItem -path $ParentDir -Recurse
 $Files | Rename-Item -NewName {$_.LastWriteTime.toString("dd-MM-yyyy-HH-mm-ss") + $_.Extension}
 
+# Extract year and month from LastWriteTime property of file then move to folders based on year and month
 foreach ($Files in $Files)
 {
     $Year = $Files.LastWriteTime.Year.ToString()
@@ -16,6 +22,6 @@ foreach ($Files in $Files)
         New-Item -Path $Directory -ItemType Directory    
     }
  
-    # Move File to new location
+    # Move file to new location
     $Files | Move-Item -Destination $Directory
 }
